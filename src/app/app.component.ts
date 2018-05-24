b3e6294fe39d169b9e2885501bad10a415251712
Christian Pernillo <cpernillo11@gmail.com>
2018-05-23 21:15:35 -0600

    OneSignal plugin config and push notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {AlertController, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { OneSignal } from '@ionic-native/onesignal';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -10,13 +11,29 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public oneSignal: OneSignal, public alertCtrl: AlertController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.handlerNotifications();
     });
+  }
+  private handlerNotifications(){
+    this.oneSignal.startInit('8380f62f-aee6-4a36-b65e-42bb4350efff', '118786345232');
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
+    this.oneSignal.handleNotificationOpened()
+      .subscribe(jsonData => {
+        let alert = this.alertCtrl.create({
+          title: jsonData.notification.payload.title,
+          subTitle: jsonData.notification.payload.body,
+          buttons: ['OK']
+        });
+        alert.present();
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+      });
+    this.oneSignal.endInit();
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GooglePlus } from "@ionic-native/google-plus";
+import {BeeperPage} from "../beeper/beeper";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  isUserLoggedIn: any = false;
+  userInfo: any = {};
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public gp: GooglePlus) {
 
   }
+  loginWithGP(){
+    this.gp.login({}).then(res => {
+      this.userInfo = res;
+      this.isUserLoggedIn = true;
+    }).catch(err => console.log('error',err));
+  }
+  gotoBeeper(){
+    this.navCtrl.push(BeeperPage);
+  }
 
+  logout(){
+    this.gp.logout().then(()=>{
+      this.isUserLoggedIn = false;
+    });
+  }
 }
